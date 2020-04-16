a5d4d950330fe38934a8de3b908e2f311b302944
李小焕 <lixiaohuan@example.com>
2020-04-16 15:18:06 +0800

    master修改

--- src/main/java/com/example/demo/SpringApplication.java
+++ src/main/java/com/example/demo/SpringApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class SpringApplication {
     public static void run(Class<DemoApplication> demoApplicationClass, String[] args) {
-        // 第一次
+        // 第一次 master修改
         // 第二次
     }
 }
