c4ffeb34c72934e2624120b2be615190f5461af5
李小焕 <lixiaohuan@example.com>
2020-04-16 14:57:10 +0800

    第二次

--- src/main/java/com/example/demo/SpringApplication.java
+++ src/main/java/com/example/demo/SpringApplication.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class SpringApplication {
     public static void run(Class<DemoApplication> demoApplicationClass, String[] args) {
         // 第一次
+        // 第二次
     }
 }
